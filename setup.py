dc6f7d5b20f0f27d49ff5e9fab97b68102ac33f1
seasidesparrow <brandewijn@gmail.com>
2018-01-24 20:10:43 -0500

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     description = ("A set of data manipulation tools useful for analyzing photometry from the AAVSO International Database."),
     keywords = "aavso photometry variable star",
     url = "https://github.com/seasidesparrow/AAVSO-analysis",
-    install_requires = ['numpy', 'pandas', 'matplotlib'],
+    install_requires = ['numpy', 'pandas', 'matplotlib']
 )
